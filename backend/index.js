4040b9f29a6f148d4e43dfb86f13316022ff55b4
zahirul <zahirulopel@gmail.com>
2024-11-18 15:53:31 +0600

    add morgan http logger

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const bookRouters = require("./routers/book");
+
+app.use(morgan("tiny"));
 
 app.use("/api/book", bookRouters);
 app.get("/", (req, res) => {
