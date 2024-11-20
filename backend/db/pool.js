0cfa5162e691b6dae56da070b1c28a0e4612474b
zahirul <zahirulopel@gmail.com>
2024-11-20 18:24:31 +0600

    add db link to backend via .env

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,5 +1,5 @@
-const { Pool } = require("pg");
 const config = require("../utils/config");
+const { Pool } = require("pg");
 
 const connectionString = config.DB_CONNECTION_STRING;
 
--- backend/db/populatedb.js
+++ backend/db/populatedb.js
@@ -1,15 +1,14 @@
+const config = require("../utils/config");
 const { Client } = require("pg");
-require("dotenv").config();
-console.log(process.env.PORT);
+console.log("config file", config);
 
 const sql = `CREATE TABLE IF NOT EXISTS authors(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 `;
 
-const connectionString = `${config.dbConnectionInfo}`;
-console.log("connection string", connectionString);
+const connectionString = config.dbConnectionInfo;
 
 async function main() {
   console.log("seeding...");
@@ -17,13 +16,11 @@ async function main() {
   const client = new Client({
     connectionString,
   });
-  try {
-    await client.connect();
-  } catch (error) {
-    console.log("connection error", error);
-  }
+
+  await client.connect();
   await client.query(sql);
   await client.end();
+
   console.log("done");
 }
 
