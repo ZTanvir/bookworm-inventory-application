0561889fa185806e9c46efce8d500d7e0a705b6f
zahirul <zahirulopel@gmail.com>
2024-12-14 17:15:47 +0600

    backend:add multure so user can upload book cover image

--- backend/routers/category.js
+++ backend/routers/category.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const categoryRoute = express.Router();
+const crypto = require("crypto");
+const multer = require("multer");
+
+// Edit file name uploaded by users
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const nameOfFile = `${crypto
+      .randomBytes(4)
+      .toString("hex")}${file.originalname.split(" ").join("")}`;
+    cb(null, nameOfFile);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const categoryController = require("../controllers/category");
 
 categoryRoute.get("/categories", categoryController.getAllCategories);
 // get a single categories
 categoryRoute.get("/category/:id", categoryController.getSingleCategory);
 // add new categories
-categoryRoute.post("/category/new", categoryController.addCategory);
+categoryRoute.post(
+  "/category/new",
+  upload.single("categoryimg"),
+  categoryController.addCategory
+);
 // update a category
 categoryRoute.put("/category/update/:id", categoryController.updateCategory);
 // delete a category
