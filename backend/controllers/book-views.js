3a4cc68ba5f7be08e172ed29e32f863adad91498
zahirul <zahirulopel@gmail.com>
2025-01-26 20:45:38 +0600

    categories page render all the categoies in html

--- backend/controllers/book-views.js
+++ backend/controllers/book-views.js
@@ -1,6 +1,26 @@
+const categoriesDb = require("../db/categoriesQuery");
+const logger = require("../utils/logger");
+
 exports.createNewCategoryGet = (req, res) => {
   res.render("pages/new-category", {
     pageTitle: "Add new category",
     errors: [],
   });
+};
+exports.getAllCategories = async (req, res) => {
+  // call db for all book categories list
+  try {
+    const { rows } = await categoriesDb.getAllCategories();
+    logger.info("List of all rows:", rows);
+    res.render("pages/categories", {
+      pageTitle: "Category List",
+      categoryList: rows,
+    });
+  } catch (error) {
+    logger.error("Book categories not found");
+    res.render("pages/categories", {
+      pageTitle: "Category List",
+      categoryList: [],
+    });
+  }
 };
--- backend/routers/books-views.js
+++ backend/routers/books-views.js
@@ -3,5 +3,6 @@ const bookViews = express.Router();
 const bookViewsController = require("../controllers/book-views");
 
 bookViews.get("/category/create", bookViewsController.createNewCategoryGet);
+bookViews.get("/categories", bookViewsController.getAllCategories);
 
 module.exports = bookViews;
