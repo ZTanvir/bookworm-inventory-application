eacea417e73f39ed2fee2ef159bc8abf6b9110c6
zahirul <zahirulopel@gmail.com>
2025-02-04 18:47:04 +0600

    only 3 table book,categories,book categories in db

--- backend/db/populatedb.js
+++ backend/db/populatedb.js
@@ -3,11 +3,6 @@ const config = require("../utils/config");
 const { Client } = require("pg");
 
 const sql = `
-CREATE TABLE IF NOT EXISTS authors(
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS categories(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -17,16 +12,12 @@ CREATE TABLE IF NOT EXISTS categories(
 CREATE TABLE IF NOT EXISTS books(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  authors TEXT NOT NULL,
   number_of_pages INT NOT NULL,
   released DATE NOT NULL,
   price INTEGER NOT NULL,
   cover_img_src TEXT NOT NULL
-);
-CREATE TABLE IF NOT EXISTS ratings(
-  id SERIAL PRIMARY KEY,
-  book_id INT NOT NULL,
-  description TEXT NOT NULL,
-  CONSTRAINT bookId FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE
 );
 CREATE TABLE IF NOT EXISTS book_categories(
   id SERIAL PRIMARY KEY,
@@ -34,13 +25,6 @@ CREATE TABLE IF NOT EXISTS book_categories(
   category_id INT NOT NULL,
   CONSTRAINT fk_book FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE,
   CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES categories(id) ON DELETE CASCADE
-);
-CREATE TABLE IF NOT EXISTS book_authors(
-  id SERIAL PRIMARY KEY,
-  book_id INT NOT NULL,
-  author_id INT NOT NULL,
-  CONSTRAINT fk_book_id FOREIGN KEY(book_id) REFERENCES books(id) ON DELETE CASCADE,
-  CONSTRAINT fk_author_id FOREIGN KEY(author_id) REFERENCES authors(id) ON DELETE CASCADE
 );
 `;
 
