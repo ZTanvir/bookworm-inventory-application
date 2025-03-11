7c47985ff549e025380c7965e8554a1a4d823f10
zahirul <zahirulopel@gmail.com>
2025-03-11 10:56:06 +0600

    change vercel enviroment variable name

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -4,9 +4,9 @@ require("dotenv").config({ path: join(__dirname, "..", ".env") });
 const PORT = process.env.PORT;
 const dbConnectionInfo =
   process.env.NODE_ENV === "production"
-    ? process.env.VERCEL_DATABASE_URL
+    ? process.env.DATABASE_URL
     : process.env.NODE_ENV === "developmentLocal"
     ? process.env.PG_CONNECTION_STRING
-    : process.env.VERCEL_DATABASE_URL;
+    : process.env.DATABASE_URL;
 
 module.exports = { PORT, dbConnectionInfo };
